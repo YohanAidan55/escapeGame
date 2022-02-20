78b8ea4e57a8be1415cfb5cf327bb810d038892d
Alex1526 <alecandre_hassan@hotmail.fr>
2022-02-20 14:03:22 +0100

    ajout du projet

---
+++ Assets/Sprite/ScriptGamePlay/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int speed = 1;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    public Animator animator;
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+}
